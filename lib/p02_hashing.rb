16cf96d14a2b958b7d6ca7e31406258e552770ed
App Academy Student <student@appacademy.io>
2017-04-07 15:13:41 -0700

    Hashing ADT implementation

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,11 +4,17 @@ end
 
 class Array
   def hash
+    hash_val = 87654
+    self.each.with_index do |el, ind|
+      hash_val ^= el.hash + ind
+    end
+    hash_val
   end
 end
 
 class String
   def hash
+    chars.map(&:ord).hash
   end
 end
 
@@ -16,6 +22,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    to_a.sort_by(&:hash).hash
   end
 end
