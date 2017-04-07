147ccb4c7563b75f91e256616b1ed113ffbe21ba
App Academy Student <student@appacademy.io>
2017-04-07 16:49:40 -0700

    Linked list implementation

--- lib/p04_linked_list.rb
+++ lib/p04_linked_list.rb
@@ -13,13 +13,22 @@ class Link
   end
 
   def remove
-    # optional but useful, connects previous link to next link
-    # and removes self from list.
+    self.prev.next = self.next if self.prev
+    self.next.prev = self.prev if self.next
+    self.prev = nil
+    self.next = nil
+    self
   end
 end
 
 class LinkedList
+  include Enumerable
+
   def initialize
+    @head = Link.new
+    @tail = Link.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -28,34 +37,66 @@ class LinkedList
   end
 
   def first
+    empty? ? nil : @head.next
   end
 
   def last
+    empty? ? nil : @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    each { |link| return link.val if link.key == key }
+    nil
   end
 
   def include?(key)
+    any? { |link| link.key == key }
   end
 
   def append(key, val)
+    new_link = Link.new(key, val)
+
+    @tail.prev.next = new_link
+    new_link.prev = @tail.prev
+    new_link.next = @tail
+    @tail.prev = new_link
+
+    new_link
   end
 
   def update(key, val)
+    each do |link|
+      if link.key == key
+        link.val = val
+        return link
+      end
+    end
   end
 
   def remove(key)
+    each do |link|
+      if link.key == key
+        link.remove
+        return link.val
+      end
+    end
+
+    nil
   end
 
   def each
+    current_link = @head.next
+    until current_link == @tail
+      yield current_link
+      current_link = current_link.next
+    end
   end
 
-  # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
