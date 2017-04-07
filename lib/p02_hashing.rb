7e7df176b6d2f441ed2495b580ac168c5215af88
App Academy Student <student@appacademy.io>
2017-04-07 15:44:18 -0700

    Implementation of hash set

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -1,7 +1,3 @@
-class Fixnum
-  # Fixnum#hash already implemented for you
-end
-
 class Array
   def hash
     hash_val = 87654
@@ -19,8 +15,7 @@ class String
 end
 
 class Hash
-  # This returns 0 because rspec will break if it returns nil
-  # Make sure to implement an actual Hash#hash method
+
   def hash
     to_a.sort_by(&:hash).hash
   end
